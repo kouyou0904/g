000734957b58cabdc784cc659df5a93256b52513
DESKTOP-I70F9GA\raiwe <sdsjhsgd@gmail.com>
2019-03-01 15:39:17 +0900

    d

---
+++ Gran_inoue/Assets/Terasurware/Classes/Editor/Status_importer.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+using System.Xml.Serialization;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+public class Status_importer : AssetPostprocessor
+{
+    private static readonly string filePath = "Assets/Resources/Date/Status.xlsx";
+    private static readonly string[] sheetNames = { "Status", };
+    
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        foreach (string asset in importedAssets)
+        {
+            if (!filePath.Equals(asset))
+                continue;
+
+            using (FileStream stream = File.Open (filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+				IWorkbook book = null;
+				if (Path.GetExtension (filePath) == ".xls") {
+					book = new HSSFWorkbook(stream);
+				} else {
+					book = new XSSFWorkbook(stream);
+				}
+
+                foreach (string sheetName in sheetNames)
+                {
+                    var exportPath = "Assets/Resources/Date/" + sheetName + ".asset";
+                    
+                    // check scriptable object
+                    var data = (Entity_Status)AssetDatabase.LoadAssetAtPath(exportPath, typeof(Entity_Status));
+                    if (data == null)
+                    {
+                        data = ScriptableObject.CreateInstance<Entity_Status>();
+                        AssetDatabase.CreateAsset((ScriptableObject)data, exportPath);
+                        data.hideFlags = HideFlags.NotEditable;
+                    }
+                    data.param.Clear();
+
+					// check sheet
+                    var sheet = book.GetSheet(sheetName);
+                    if (sheet == null)
+                    {
+                        Debug.LogError("[QuestData] sheet not found:" + sheetName);
+                        continue;
+                    }
+
+                	// add infomation
+                    for (int i=1; i<= sheet.LastRowNum; i++)
+                    {
+                        IRow row = sheet.GetRow(i);
+                        ICell cell = null;
+                        
+                        var p = new Entity_Status.Param();
+			
+					cell = row.GetCell(0); p.ID = (cell == null ? "" : cell.StringCellValue);
+					cell = row.GetCell(1); p.NAME = (cell == null ? "" : cell.StringCellValue);
+					cell = row.GetCell(2); p.HP = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(3); p.Att = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(4); p.Def = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(5); p.Matt = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(6); p.Mdef = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(7); p.Imap = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(8); p.Wei = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(9); p.Tec = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(10); p.Avo = (int)(cell == null ? 0 : cell.NumericCellValue);
+
+                        data.param.Add(p);
+                    }
+                    
+                    // save scriptable object
+                    ScriptableObject obj = AssetDatabase.LoadAssetAtPath(exportPath, typeof(ScriptableObject)) as ScriptableObject;
+                    EditorUtility.SetDirty(obj);
+                }
+            }
+
+        }
+    }
+}
---
+++ Gran_inoue/Assets/Terasurware/Classes/Entity_StatusReset.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Entity_StatusReset : ScriptableObject
+{	
+	public List<Param> param = new List<Param> ();
+
+	[System.SerializableAttribute]
+	public class Param
+	{
+		
+		public string ID;
+		public string NAME;
+		public int HP;
+		public int Att;
+		public int Def;
+		public int Matt;
+		public int Mdef;
+		public int Imap;
+		public int Wei;
+		public int Tec;
+		public int Avo;
+	}
+}
